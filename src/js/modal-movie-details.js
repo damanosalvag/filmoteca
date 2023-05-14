24c421743e16582768fd9cd2a425a204eb79b498
Dairo Manosalva <38769711+damanosalvag@users.noreply.github.com>
2023-05-13 19:07:05 -0500

    fix genres modal

--- src/js/modal-movie-details.js
+++ src/js/modal-movie-details.js
@@ -11,7 +11,14 @@ const {
 export async function detailsMovieValues(id_movie) {
     const movieInfoObj = await getAPI.detailMovie(id_movie);
     const { backdrop_path, poster_path, original_title, title, vote_average, vote_count, popularity, genres, overview, id } = movieInfoObj.data;
-    console.log(backdrop_path);
+    
+    // Destructuration object genres to array of names
+    let desGenres = '';
+    const gen = movieInfoObj.data.genres;
+        gen.forEach(element => {
+            desGenres += element.name+' | ';
+    });
+
     //image poster
     if (poster_path != null) {
         modalImgPoster.src = `https://image.tmdb.org/t/p/w500${poster_path}`;
@@ -27,7 +34,7 @@ export async function detailsMovieValues(id_movie) {
         <li>${vote_average}/${vote_count}</li>
         <li>${popularity}</li>
         <li>${original_title}</li>
-        <li>${genres.name}</li>`
+        <li>${desGenres}</li>`
     modalInfoValues.innerHTML = markup;
     // Description
     modalDescMovie.textContent = overview;
